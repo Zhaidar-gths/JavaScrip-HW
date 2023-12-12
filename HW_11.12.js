2541681e1c5d1836f1f74e37fd78b7696688be95
Zhaidar <149644540+Zhaidar-gths@users.noreply.github.com>
2023-12-12 08:01:35 +0600

    HW 12.12

---
+++ HW_11.12.js
@@ -0,0 +1,16 @@
+// Home Work 11.12.23
+
+// String Task
+// task1
+let name='Жайдар'
+let favoriteColor='Розовый'
+let favoriteSong='Stay'
+let favoriteMovie='Приключения Паддингтона'
+let favoriteAnimal='Cats'
+le.log(`Менің атым ${name} .Менің жақсы көретін түсім ${favoriteColor}. Мен ${favoriteSong}деген музыканы жақсы көремін. Маған көптеген филмдар ұнайды соның ішінде ${favoriteMovie}
+conso жақсы көремін. Сонымен қатар маған жануарлардан  ${favoriteAnimal} ұнайды. `)
+
+
+
+// Task2
+ let isWeekend 
